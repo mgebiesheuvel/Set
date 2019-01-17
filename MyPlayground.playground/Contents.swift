41be964c67bd33b5d712f761a753f5b9671a8bbf
Marten Biesheuvel <martenbiesheuvel@outlook.com>
2019-01-17 23:25:45 +0100

    Remove Concentration as this repo is for Set only

--- MyPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-extension Int: Sequence {
-    public func makeIterator() -> CountableRange<Int>.Iterator {
-        return (0..<self).makeIterator()
-    }
-}
- 
- for index in 3 {
-    print(index)
- }
